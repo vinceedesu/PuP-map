08c56392ecb9ca861aeed05e3a842fbbd55cf7dc
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2023-01-03 22:03:10 +0800

    asd

---
+++ script.js
@@ -0,0 +1,25 @@
+function showSceneryBox(imgScource, heading, description){
+    let d = document.getElementById("display");
+    let e = document.getElementById("body");
+    e.style.overflow = "hidden";
+    document.addEventListener('mousemove', displayFollowCamera);
+    d.style.left = event.clientX + 5 + "px";
+    d.style.top = event.clientY + 5 + "px";
+    d.innerHTML = "<div id='wrapper'><img id='imgid' src= '" + imgScource + "' >" + "<p>" + heading + "</p>" + "<p>" + description +"</p></div>";
+    
+}
+
+function displayFollowCamera(){
+    let a = document.getElementById('display');
+    a.style.left = event.clientX + 5 + "px";
+    a.style.top = event.clientY + 5 + "px";
+}
+
+function removeSceneryBox(){
+    let a = document.getElementById("display");
+    let b = document.getElementById("body");
+    a.removeAttribute('style');
+    b.removeAttribute('style');
+    document.removeEventListener('mousemove', displayFollowCamera);
+    a.removeChild(a.childNodes[0]);
+}
