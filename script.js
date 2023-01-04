9e70ffbb4553640c723d90de5341e6b17ee48a39
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2023-01-04 20:55:25 +0800

    finalize

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-
-
-function showSceneryBox(imgScource, heading, description){
-    let d = document.getElementById("display");
-    let e = document.getElementById("body");
-    e.style.overflow = "hidden";
+function showSceneryBox(imgSource, heading, description){
+    a = document.getElementById('display');
+    a.innerHTML = `<div id='wrapper'><img id = 'imgid' src="${imgSource}" height="200" width="350"><h3>${heading}</h3><p>${description}</p></div>`;
+    a.style.display = 'block';
     document.addEventListener('mousemove', displayFollowCamera);
-    d.style.left = event.clientX + 5 + "px";
-    d.style.top = event.clientY + 5 + "px";
-    d.innerHTML = "<div id='wrapper'><img id='imgid' src='" + imgScource + "'>" + "<p>" + heading  + "</p>" + description + "</p></div>";
-}
-function displayFollowCamera(){
-    let a = document.getElementById('display');
-    a.style.left = event.clientX + 5 + "px";
-    a.style.top = event.clientY + 5 + "px";
 }
 
-function removeSceneryBox(){
-    let a = document.getElementById("display");
-    let b = document.getElementById("body");
-    a.removeAttribute('style');
-    b.removeAttribute('style');
-    document.removeEventListener('mousemove', displayFollowCamera);
-    a.removeChild(a.childNodes[0]);
+function displayFollowCamera() {
+    wrapper = document.getElementById('display');
+    wrapper.style.top = `${event.clientY + 2}px`;
+    wrapper.style.left = `${event.clientX + 2}px`;
 }
+
+function removeSceneryBox() {
+    a = document.getElementById('display');
+    a.style.display = 'none';
+    document.removeEventListener('mousemove', displayFollowCamera);
+}            
